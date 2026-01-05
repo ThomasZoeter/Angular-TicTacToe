656ef0b534434357cfc7639e8a251ff82c381351
Thomas Zoeter <thomas.zoeter@keylane.com>
2026-01-05 16:45:24 +0100

    Add clickable fields?

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,23 @@ import { Component } from '@angular/core';
   template: `
       <h3>GAME!</h3>
       <div id="fields" class="playfield">
-          <div id="field1" class="field">1</div>
-          <div id="field2" class="field">2</div>
-          <div id="field3" class="field">3</div>
-          <div id="field4" class="field">4</div>
-          <div id="field5" class="field">5</div>
-          <div id="field6" class="field">6</div>
-          <div id="field7" class="field">7</div>
-          <div id="field8" class="field">8</div>
-          <div id="field9" class="field">9</div>
+          <div id="field1" class="field" (click)="fieldClicked()">{{ clicked || '' }}</div>
+          <div id="field2" class="field">{{ clicked }}</div>
+          <div id="field3" class="field">{{ clicked }}</div>
+          <div id="field4" class="field">{{ clicked }}</div>
+          <div id="field5" class="field">{{ clicked }}</div>
+          <div id="field6" class="field">{{ clicked }}</div>
+          <div id="field7" class="field">{{ clicked }}</div>
+          <div id="field8" class="field">{{ clicked }}</div>
+          <div id="field9" class="field">{{ clicked }}</div>
       </div>
   `
 })
 export class App {
-  name = 'World';
+  clicked = '';
+  fieldClicked() {
+    this.clicked = this.clicked ? 'X' : 'O'
+  }
 }
 
 bootstrapApplication(App);
