7bfbfb682ca4616730700bc9eaf7d5658d9efecb
Thomas Zoeter <thomas.zoeter@keylane.com>
2026-01-05 20:40:30 +0100

    Try to rewrite field of divs

--- src/main.ts
+++ src/main.ts
@@ -23,27 +23,43 @@ export class ClickColorDirective {
   }
 }
 
+@Directive({
+  standalone: true,
+  selector: '[clicked]'
+})
+export class ClickDirective {
+
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, ClickColorDirective],
   template: `
       <h3>GAME!</h3>
-      <div id="fields" class="playfield">
-          <div id="field1" class="field" clickColor >{{ clicked }}</div>
-          <div id="field2" class="field" clickColor [style.background-color]="'lightblue'">{{ clicked }}</div>
-          <div id="field3" class="field">{{ clicked }}</div>
-          <div id="field4" class="field">{{ clicked }}</div>
-          <div id="field5" class="field">{{ clicked }}</div>
-          <div id="field6" class="field">{{ clicked }}</div>
-          <div id="field7" class="field">{{ clicked }}</div>
-          <div id="field8" class="field">{{ clicked }}</div>
-          <div id="field9" class="field">{{ clicked }}</div>
+      <!--      <div id="fields" class="playfield">-->
+      <!--          <div id="field1" class="field" clickColor >{{ clicked }}</div>-->
+      <!--          <div id="field2" class="field" clickColor [style.background-color]="'lightblue'">{{ clicked }}</div>-->
+      <!--          <div id="field3" class="field">{{ clicked }}</div>-->
+      <!--          <div id="field4" class="field">{{ clicked }}</div>-->
+      <!--          <div id="field5" class="field">{{ clicked }}</div>-->
+      <!--          <div id="field6" class="field">{{ clicked }}</div>-->
+      <!--          <div id="field7" class="field">{{ clicked }}</div>-->
+      <!--          <div id="field8" class="field">{{ clicked }}</div>-->
+      <!--          <div id="field9" class="field">{{ clicked }}</div>-->
+      <!--      </div>-->
+      <div #divs class="playfield">
+          @for (item of items;track item) {
+              <div [id]="'div-' + item"> Div {{ item }}</div>
+          }
       </div>
   `
 })
 export class App {
+  items = [1,2,3,4,5,6,7,8,9]
   clicked = 'X'
+  currentPlayer = 'X'
+  opponent = this.currentPlayer == 'X' ? 'O' : 'X'
   color = 'lightblue'
   // fieldClicked() {
   //   this.clicked = 'X'
