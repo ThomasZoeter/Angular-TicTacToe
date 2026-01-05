e3610e2c9132024ad4c9eebb4c6cd7198baebf7e
Thomas Zoeter <thomas.zoeter@keylane.com>
2026-01-05 21:13:26 +0100

    Print index of div (new try)

--- src/main.ts
+++ src/main.ts
@@ -67,13 +67,13 @@ export class ClickDirective {
 })
 export class App {
   items = [1,2,3,4,5,6,7,8,9]
-  @ViewChildren("divs") divs: QueryList<ElementRef>;
+  @ViewChildren("divs") divlist: QueryList<any>;
   clicked = 'X'
   currentPlayer = 'X'
   opponent = this.currentPlayer == 'X' ? 'O' : 'X'
   color = 'lightblue'
   fieldClicked(index: number) {
-    console.log(this.divs.get(index))
+    console.log(this.divlist.get(index))
     }
 }
 
